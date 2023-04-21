cdddc4ab6abd30131aa4927146be32d37cdc1e2d
BrunoSarmento98 <122676180+BrunoSarmento98@users.noreply.github.com>
2023-04-21 10:23:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
-
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Esse é o trabalho final do Grupo 28"
 
 if __name__ == '__main__':
     app.run()
