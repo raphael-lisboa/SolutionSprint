78e59c4a90f05800a9f61040b6105d65bd440c0a
BrunoSarmento98 <122676180+BrunoSarmento98@users.noreply.github.com>
2023-04-21 21:54:25 -0300

    Update test.py

--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Esse é o trabalho final do Grupo 28")
+        self.assertEqual(self.result.data.decode('utf-8'), "Esse é o trabalho final do Grupo 28 V2")
